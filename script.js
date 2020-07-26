71011338fbb14cd33b10a88b172ccb6acb9fb077
Ben Parsons <ben@bpulse.co.uk>
2020-07-26 13:15:10 +0100

    improve prompt

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function main(days) {
     function onMapClick(e) {
         popup
             .setLatLng(e.latlng)
-            .setContent(`[${e.latlng.lat.toFixed(6)},${e.latlng.lng.toFixed(6)}]`)
+            .setContent(`[${e.latlng.lat.toFixed(6)}, ${e.latlng.lng.toFixed(6)}, ""]`)
             .openOn(mymap);
     }
     mymap.on('click', onMapClick);
